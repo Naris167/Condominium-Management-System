16385b06bd64b4e67ad26039d7b18c5ca471649f
Naris167 <68007430+Naris167@users.noreply.github.com>
2022-08-31 13:21:56 +0700

    W3T1 - Authenticate User and Login Screen

--- src/main/java/org/stamford/mainApp.java
+++ src/main/java/org/stamford/mainApp.java
@@ -16,20 +16,24 @@ public class mainApp extends Application {
         //Creating a Stack Pane
         StackPane pane = new StackPane();
 
-        Label currentUserLabel = new Label("Current User: " + "None");
-        Label systemMsgLabel = new Label("You must login before using Stamford-CMS");
+        String myClass = getClass().toString();
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
 
-        currentUserLabel.setId("currentUserLabel");
-        systemMsgLabel.setId("systemMsgLabel");
-        VBox vbox = new VBox(10);
-        vbox.getChildren().add(currentUserLabel);
-        vbox.getChildren().add(systemMsgLabel);
+//        Label currentUserLabel = new Label("Current User: " + "None");
+//        Label systemMsgLabel = new Label("You must login before using Stamford-CMS");
+//
+//        currentUserLabel.setId("currentUserLabel");
+//        systemMsgLabel.setId("systemMsgLabel");
+//        VBox vbox = new VBox(10);
+//        vbox.getChildren().add(currentUserLabel);
+//        vbox.getChildren().add(systemMsgLabel);
 
         // Create a new scene
-        Scene scene = new Scene(vbox);
+        //Scene scene = new Scene(vbox);
+        Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         // primary stage is created by the platform (JavaFX) already
-        stage.setTitle("Stamford-CMS");
+        stage.setTitle("Stamford-CMS V1.0");
         stage.setScene(scene);
         stage.show();
         System.out.println("scene.fxml file URL is: " + this.getClass().getResource("scene.fxml").toExternalForm());
